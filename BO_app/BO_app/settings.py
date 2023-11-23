ba2f96187218a63baeae2dd6b413d063f9829834
BuildTools <joschkamayer@gmail.com>
2023-11-23 11:08:21 +0100

    Creating app

--- BO_app/BO_app/settings.py
+++ BO_app/BO_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_=qh+u3b32ciiz@zq!7hvy5!@&=g7=(loa^j5le8p&5z8cv9=i'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
